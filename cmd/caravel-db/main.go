9bc37ed833e99a32c03931be2a2d0b8bf37add5c
Aerodos12 <weldon.henson97@gmail.com>
2022-07-15 13:06:13 -0400

    Setup

--- cmd/caravel-db/main.go
+++ cmd/caravel-db/main.go
@@ -3,8 +3,9 @@ package main
 import (
  "fmt"
  "os"
- tea "github.com/charmbracelet/bubbletea"
-);
+
+  tea "github.com/charmbracelet/bubbletea"
+)
 
 type CaravelPackage struct {
     id int
@@ -32,10 +33,30 @@ func initialModel() CaravelDBModel {
 
 
 func (m CaravelDBModel) Init() tea.Cmd {
-
     return nil;
 }
 
-func main(){
+func (m CaravelDBModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+        case tea.KeyMsg:
+            switch msg.String() {
+                case "ctrl+q":
+                    return m, tea.Quit;
+            }
+    }
+    return m, nil
+}
 
+func (m CaravelDBModel) View() string {
+    s := "CaravelDB Editor"
+
+    return s
+}
+
+func main(){
+    p := tea.NewProgram(initialModel())
+    if err := p.Start(); err != nil {
+        fmt.Printf("Error detected (%v). Exiting...", err);
+         os.Exit(1);
+    }
 }
